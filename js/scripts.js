0f4c6a403ea30995bdeb6a01c0953f686e58a232
Kate <katerina.rodnykh@yandex.ru>
2020-12-13 18:56:09 +0300

    Прайс-лист, блок "акции и предложения на главной"

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,52 @@ hamburger.addEventListener("click", function (evt) {
     navigation.classList.toggle("open");
     header.classList.toggle("open");
 });
+
+/* Аккордеон */
+
+var accordion = (function (element) {
+    var _getItem = function (elements, className) {
+        var element = undefined;
+        elements.forEach(function (item) {
+            if (item.classList.contains(className)) {
+                element = item;
+            }
+        });
+        return element;
+    };
+    return function () {
+        var _mainElement = ('.accordion'),
+            _items = ('.accordion-item'),
+            _contents = ('.accordion-item-content');
+        var _actionClick = function (e) {
+                if (!e.target.classList.contains('accordion-item-header')) {
+                    return;
+                }
+                e.preventDefault();
+                var header = e.target,
+                    item = header.parentElement,
+                    itemActive = _getItem(_items, 'show');
+                if (itemActive === undefined) {
+                    item.classList.add('show');
+                } else {
+                    itemActive.classList.remove('show');
+                    if (itemActive !== item) {
+                        item.classList.add('show');
+                    }
+                }
+            },
+            _setupListeners = function () {
+                _mainElement.addEventListener('click', _actionClick);
+            };
+
+        return {
+            init: function (element) {
+                _mainElement = (typeof element === 'string' ? document.querySelector(element) : element);
+                _items = _mainElement.querySelectorAll('.accordion-item');
+                _setupListeners();
+            }
+        }
+    }
+})();
+var accordion1 = accordion();
+accordion1.init('#accordion');
